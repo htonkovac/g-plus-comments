23fe55e470195e319a0db201e98b0b4a566f7756
Hrvoje Tonkovac <tonkovac@uhp-digital.com>
2017-07-10 15:54:48 +0200

    inddex

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head></head>
+<body>
+ <script src="https://apis.google.com/js/plusone.js" gapi-processed="true"></script>
+                        <div id="comments"></div>
+                        <script>
+                            gapi.comments.render('comments', {
+                                href: window.location,
+                                width: '624',
+                                first_party_property: 'BLOGGER',
+                                view_type: 'FILTERED_POSTMOD'
+                            });
+                        </script>
+
+</body>
